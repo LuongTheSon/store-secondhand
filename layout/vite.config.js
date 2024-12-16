c14a75c1b6eeb61b1467ff62b637ef96ba890adf
LuongTheSon <thesonworking@gmail.com>
2024-12-16 11:02:19 +0700

    Fix asset paths for GitHub Pages

--- layout/vite.config.js
+++ layout/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
-  base: '/store-secondhand/layout/',
+  base: '/store-secondhand/',
   plugins: [react()],
   resolve: {
     alias: {
