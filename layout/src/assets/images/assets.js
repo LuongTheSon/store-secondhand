6012a752232203c129dbf22a972c55ac3ac33bfc
TheSon <thesonworking@gmail.com>
2026-01-19 17:01:22 +0700

    [fix]: fix assets path image

--- layout/src/assets/images/assets.js
+++ layout/src/assets/images/assets.js
@@ -1,3 +1,120 @@
+import p_img1_1 from './p_img1_1.png';
+import p_img1_2 from './p_img1_2.png';
+import p_img1_3 from './p_img1_3.png';
+import p_img1_4 from './p_img1_4.png';
+import p_img2_1 from './p_img2_1.png';
+import p_img2_2 from './p_img2_2.png';
+import p_img2_3 from './p_img2_3.png';
+import p_img2_4 from './p_img2_4.png';
+import p_img3_1 from './p_img3_1.png';
+import p_img3_2 from './p_img3_2.png';
+import p_img3_3 from './p_img3_3.png';
+import p_img3_4 from './p_img3_4.png';
+import p_img4_1 from './p_img4_1.png';
+import p_img4_2 from './p_img4_2.png';
+import p_img4_3 from './p_img4_3.png';
+import p_img4_4 from './p_img4_4.png';
+import p_img5_1 from './p_img5_1.png';
+import p_img5_2 from './p_img5_2.png';
+import p_img5_3 from './p_img5_3.png';
+import p_img5_4 from './p_img5_4.png';
+import p_img6_1 from './p_img6_1.png';
+import p_img6_2 from './p_img6_2.png';
+import p_img6_3 from './p_img6_3.png';
+import p_img6_4 from './p_img6_4.png';
+import p_img7_1 from './p_img7_1.png';
+import p_img7_2 from './p_img7_2.png';
+import p_img7_3 from './p_img7_3.png';
+import p_img7_4 from './p_img7_4.png';
+import p_img8_1 from './p_img8_1.png';
+import p_img8_2 from './p_img8_2.png';
+import p_img8_3 from './p_img8_3.png';
+import p_img8_4 from './p_img8_4.png';
+import p_img9_1 from './p_img9_1.png';
+import p_img9_2 from './p_img9_2.png';
+import p_img9_3 from './p_img9_3.png';
+import p_img9_4 from './p_img9_4.png';
+import p_img10_1 from './p_img10_1.png';
+import p_img10_2 from './p_img10_2.png';
+import p_img10_3 from './p_img10_3.png';
+import p_img10_4 from './p_img10_4.png';
+import p_img11_1 from './p_img11_1.png';
+import p_img11_2 from './p_img11_2.png';
+import p_img11_3 from './p_img11_3.png';
+import p_img11_4 from './p_img11_4.png';
+import p_img12_1 from './p_img12_1.png';
+import p_img12_2 from './p_img12_2.png';
+import p_img12_3 from './p_img12_3.png';
+import p_img12_4 from './p_img12_4.png';
+import p_img13_1 from './p_img13_1.png';
+import p_img13_2 from './p_img13_2.png';
+import p_img13_3 from './p_img13_3.png';
+import p_img13_4 from './p_img13_4.png';
+import p_img14_1 from './p_img14_1.png';
+import p_img14_2 from './p_img14_2.png';
+import p_img14_3 from './p_img14_3.png';
+import p_img14_4 from './p_img14_4.png';
+import p_img15_1 from './p_img15_1.png';
+import p_img15_2 from './p_img15_2.png';
+import p_img15_3 from './p_img15_3.png';
+import p_img15_4 from './p_img15_4.png';
+import p_img16_1 from './p_img16_1.png';
+import p_img16_2 from './p_img16_2.png';
+import p_img16_3 from './p_img16_3.png';
+import p_img16_4 from './p_img16_4.png';
+import p_img17_1 from './p_img17_1.png';
+import p_img17_2 from './p_img17_2.png';
+import p_img17_3 from './p_img17_3.png';
+import p_img17_4 from './p_img17_4.png';
+import p_img18_1 from './p_img18_1.png';
+import p_img18_2 from './p_img18_2.png';
+import p_img18_3 from './p_img18_3.png';
+import p_img18_4 from './p_img18_4.png';
+import p_img19_1 from './p_img19_1.png';
+import p_img19_2 from './p_img19_2.png';
+import p_img19_3 from './p_img19_3.png';
+import p_img19_4 from './p_img19_4.png';
+import p_img20_1 from './p_img20_1.png';
+import p_img20_2 from './p_img20_2.png';
+import p_img20_3 from './p_img20_3.png';
+import p_img20_4 from './p_img20_4.png';
+import p_img21 from './p_img21.png';
+import p_img22 from './p_img22.png';
+import p_img23 from './p_img23.png';
+import p_img24 from './p_img24.png';
+import p_img25 from './p_img25.png';
+import p_img26 from './p_img26.png';
+import p_img27 from './p_img27.png';
+import p_img28 from './p_img28.png';
+import p_img29 from './p_img29.png';
+import p_img30 from './p_img30.png';
+import p_img31 from './p_img31.png';
+import p_img32 from './p_img32.png';
+import p_img33 from './p_img33.png';
+import p_img34 from './p_img34.png';
+import p_img35 from './p_img35.png';
+import p_img36 from './p_img36.png';
+import p_img37 from './p_img37.png';
+import p_img38 from './p_img38.png';
+import p_img39 from './p_img39.png';
+import p_img40 from './p_img40.png';
+import p_img41 from './p_img41.png';
+import p_img42 from './p_img42.png';
+import p_img43 from './p_img43.png';
+import p_img44 from './p_img44.png';
+import p_img45 from './p_img45.png';
+import p_img46 from './p_img46.png';
+import p_img47 from './p_img47.png';
+import p_img48 from './p_img48.png';
+import p_img49 from './p_img49.png';
+import p_img50 from './p_img50.png';
+import p_img51 from './p_img51.png';
+import p_img52 from './p_img52.png';
+import p_img53 from './p_img53.png';
+import p_img54 from './p_img54.png';
+import p_img55 from './p_img55.png';
+import p_img56 from './p_img56.png';
+
 import logo from './header-logo.png';
 import banner_home1 from './banner-home-1.jpg';
 import banner_home2 from './banner-home-2.jpg';
@@ -53,4 +170,114 @@ export const bannerHome = [
   {
     image: banner_home3
   }
+];
+
+export const productImages = [
+    p_img1_1,
+    p_img1_2,
+    p_img1_3,
+    p_img1_4,
+    p_img2_1,
+    p_img2_2,
+    p_img2_3,
+    p_img2_4,
+    p_img3_1,
+    p_img3_2,
+    p_img3_3,
+    p_img3_4,
+    p_img4_1,
+    p_img4_2,
+    p_img4_3,
+    p_img4_4,
+    p_img5_1,
+    p_img5_2,
+    p_img5_3,
+    p_img5_4,
+    p_img6_1,
+    p_img6_2,
+    p_img6_3,
+    p_img6_4,
+    p_img7_1,
+    p_img7_2,
+    p_img7_3,
+    p_img7_4,
+    p_img8_1,
+    p_img8_2,
+    p_img8_3,
+    p_img8_4,
+    p_img9_1,
+    p_img9_2,
+    p_img9_3,
+    p_img9_4,
+    p_img10_1,
+    p_img10_2,
+    p_img10_3,
+    p_img10_4,
+    p_img11_1,
+    p_img11_2,
+    p_img11_3,
+    p_img11_4,
+    p_img12_1,
+    p_img12_2,
+    p_img12_3,
+    p_img12_4,
+    p_img13_1,
+    p_img13_2,
+    p_img13_3,
+    p_img13_4,
+    p_img14_1,
+    p_img14_2,
+    p_img14_3,
+    p_img14_4,
+    p_img15_1,
+    p_img15_2,
+    p_img15_3,
+    p_img15_4,
+    p_img16_1,
+    p_img16_2,
+    p_img16_3,
+    p_img16_4,
+    p_img17_1,
+    p_img17_2,
+    p_img17_3,
+    p_img17_4,
+    p_img18_1,
+    p_img18_2,
+    p_img18_3,
+    p_img18_4,
+    p_img19_1,
+    p_img19_2,
+    p_img19_3,
+    p_img19_4,
+    p_img20_1,
+    p_img20_2,
+    p_img20_3,
+    p_img20_4,
+    p_img21,
+    p_img22,
+    p_img23,
+    p_img24,
+    p_img25,
+    p_img26,
+    p_img27,
+    p_img28,
+    p_img29,
+    p_img30,
+    p_img31,
+    p_img32,
+    p_img33,
+    p_img43,
+    p_img44,
+    p_img45,
+    p_img46,
+    p_img47,
+    p_img48,
+    p_img49,
+    p_img50,
+    p_img51,
+    p_img52,
+    p_img53,
+    p_img54,
+    p_img55,
+    p_img56,
 ];
